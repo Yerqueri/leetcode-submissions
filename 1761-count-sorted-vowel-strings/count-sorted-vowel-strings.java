fffe0ecbf2621c2131c3861c5b1090c382151837
Ayush Yadav <yayush13@gmail.com>
2023-12-09 19:06:19 +0530

    Time: 0 ms (100.00%) | Memory: 39.8 MB (12.01%) - LeetSync

--- 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.java
+++ 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.java
@@ -1,19 +1,26 @@
 class Solution {
-    int totalCount =0;
+    Integer[][] dp;
     public int countVowelStrings(int n) {
+        dp=new Integer[n][5];
+        int sol =0;
         for(int i=0;i<5;i++){
-            test(i,0,n-1);
+            sol+=test(i,0,n-1);
         }
-        return totalCount;
+        return sol;
     }
 
-    private void test(int curr, int length, int n){
+    private int test(int curr, int length, int n){
         if(length==n){
-            totalCount++;
-            return;
-        }
-        for(int i=curr;i<5;i++){
-            test(i,length+1,n);
+            return 1;
+        }else if(dp[length][curr]!=null){
+            return dp[length][curr];
+        }else{
+            int count =0;
+            for(int i=curr;i<5;i++){
+                count+=test(i,length+1,n);
+            }
+            dp[length][curr] = count;
+            return count;
         }
     }
 }
