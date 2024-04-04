2abbdb4dec1d099e01509508b36f89575b603f98
Ayush Yadav <yayush13@gmail.com>
2024-04-04 11:51:36 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (40.14%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
@@ -5,7 +5,9 @@ class Solution {
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='('){
                 count++;
-                max = Math.max(max,count);
+                if(count>max){
+                    max = count;
+                }
             }else if(s.charAt(i)==')'){
                 count--;
             }
