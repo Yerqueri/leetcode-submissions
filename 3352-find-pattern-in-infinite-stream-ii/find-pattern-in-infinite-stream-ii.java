42b82aaa4859ae99ec6ec812211508db089f73d3
Ayush Yadav <yayush13@gmail.com>
2024-07-30 18:46:23 +0530

    Time: 10 ms (100.00%) | Memory: 61.1 MB (69.23%) - LeetSync

---
+++ 3352-find-pattern-in-infinite-stream-ii/find-pattern-in-infinite-stream-ii.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for an infinite stream.
+ * class InfiniteStream {
+ *     public InfiniteStream(int[] bits);
+ *     public int next();
+ * }
+ */
+class Solution {
+    public int findPattern(InfiniteStream stream, int[] pattern) {
+        long p = 0;
+        for(int i:pattern){
+            p = p<<1;
+            p = p | i;
+        }
+        int n = pattern.length;
+        long s = 0;
+        long mask = 0;
+        for(int i=0;i<n;i++){
+            s=s<<1;
+            s = s | stream.next();
+            mask = mask<<1;
+            mask = mask | 1;
+        }
+        int index =0;
+        while(s!=p){
+            s = s<<1;
+            s = s & mask;
+            s = s | stream.next();
+            index++;
+        }
+        return index;
+    }
+}
