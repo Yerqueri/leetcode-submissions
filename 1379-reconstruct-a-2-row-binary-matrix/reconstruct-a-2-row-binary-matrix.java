5fcd23a757b9688e250bd7208dcc16ef0c134596
Ayush Yadav <yayush13@gmail.com>
2024-08-28 14:46:36 +0530

    Time: 8 ms (77.23%) | Memory: 59.7 MB (61.78%) - LeetSync

--- 1379-reconstruct-a-2-row-binary-matrix/reconstruct-a-2-row-binary-matrix.java
+++ 1379-reconstruct-a-2-row-binary-matrix/reconstruct-a-2-row-binary-matrix.java
@@ -1,62 +1,39 @@
 class Solution {
     public List<List<Integer>> reconstructMatrix(int upper, int lower, int[] colsum) {
-        List<Integer> l1 = new ArrayList<>();
-        List<Integer> l2 = new ArrayList<>();
-        int[] remaining = fillTwosAndZeroes(l1,l2,colsum,upper,lower);
-        upper = remaining[0];
-        lower = remaining[1];
-        int count = remaining[2];
-        if(count != lower+upper || lower<0 || upper<0){
-            return new ArrayList<>();
-        }else{
-            construct2(l1,l2,upper);
-        }
-        return List.of(l1,l2);
-    }
-
-    private void construct2(List<Integer> l1, List<Integer> l2, int upper) {
-        for(int i=0;i<l1.size();i++){
-            if(l1.get(i).equals(-1)){
-                if(upper>0){
-                    l1.set(i,1);
-                    upper--;
-                }else{
-                    l1.set(i,0);
-                }
-            }
-        }
-        constructSecondBasedOnFirst(l1, l2);
-    }
-
-    private void constructSecondBasedOnFirst(List<Integer> l1, List<Integer> l2) {
-        for(int i = 0; i< l2.size(); i++){
-            if(l2.get(i).equals(-1)){
-                if(l1.get(i).equals(0)){
-                    l2.set(i,1);
-                }else{
-                    l2.set(i,0);
-                }
-            }
-        }
-    }
-    
-    private int[] fillTwosAndZeroes(List<Integer> l1, List<Integer> l2, int[] colsum, int upper, int lower) {
-        int count = 0;
+        int twice = 0;
+        int sum =0;
         for(int i=0;i<colsum.length;i++){
             if(colsum[i]==2){
-                l1.add(1);
-                l2.add(1);
-                upper--;
-                lower--;
-            }else if(colsum[i]==0){
-                l1.add(0);
-                l2.add(0);
-            }else{
-                l1.add(-1);
-                l2.add(-1);
-                count++;
+                twice++;
             }
+            sum+=colsum[i];
         }
-        return new int[]{upper,lower,count};
+        if(lower<twice || upper<twice || sum!=lower+upper){
+            return new ArrayList<>();
+        }else{
+            List<Integer> l1 = new ArrayList<>();
+            List<Integer> l2 = new ArrayList<>();
+            for(int i=0;i<colsum.length;i++){
+                if(colsum[i]==0){
+                    l1.add(0);
+                    l2.add(0);
+                }else if(colsum[i]==2){
+                    l1.add(1);
+                    l2.add(1);
+                    lower--;
+                    upper--;
+                    twice--;
+                }else if(upper>twice){
+                    l1.add(1);
+                    l2.add(0);
+                    upper--;
+                }else{
+                    l1.add(0);
+                    l2.add(1);
+                    lower--;
+                }
+            }
+            return List.of(l1,l2);
+        }
     }
 }
